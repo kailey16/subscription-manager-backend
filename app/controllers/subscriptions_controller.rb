f4bd1c9fad51bd74d7d7d63e90ef8957098399c8
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-02-19 12:32:26 -0500

    controllers set up

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,5 @@
+class SubscriptionsController < ApplicationController
+  before_action :authorized
+
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :subscriptions
   
+  validates :username, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
 end
